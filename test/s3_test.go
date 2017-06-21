797ae4d75c7452feb9fd08bab47a7d40c09b80b5
Kyriakos Kentzoglanakis <k.kentzoglanakis@pamediakopes.gr>
2017-06-21 18:43:02 +0300

    Adds test for s3's write stream method

--- test/s3_test.go
+++ test/s3_test.go
@@ -2,11 +2,13 @@ package test
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"testing"
 
 	"github.com/e-travel/cloudwatchlogsbeat/beater"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -25,6 +27,12 @@ func (client *mockS3Client) GetObject(input *s3.GetObjectInput) (*s3.GetObjectOu
 	return stubGetObject(input)
 }
 
+var stubPutObject func(*s3.PutObjectInput) (*s3.PutObjectOutput, error)
+
+func (client *mockS3Client) PutObject(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+	return stubPutObject(input)
+}
+
 // this is our mock S3 body object
 type S3ItemBody struct {
 	io.Reader
@@ -36,6 +44,7 @@ var group = &beater.Group{
 	Name: "group",
 }
 var stream = &beater.Stream{
+	Name:   "stream",
 	Group:  group,
 	Params: &cloudwatchlogs.GetLogEventsInput{},
 }
@@ -77,6 +86,31 @@ func Test_ReadStreamInfo_WhenItemExists_ShouldUpdateStream(t *testing.T) {
 	assert.Equal(t, "This is the buffer", stream.Buffer.String())
 }
 
-func Test_WriteStreamInfo(t *testing.T) {
-	t.Skip("Pending")
+func Test_WriteStreamInfo_ShouldReturnNil_OnSuccess(t *testing.T) {
+	stream.Buffer = *bytes.NewBufferString("This is the buffer")
+	stream.Params.NextToken = aws.String("abcde")
+	stubPutObject = func(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+		body := &bytes.Buffer{}
+		body.ReadFrom(input.Body)
+		assert.Equal(t, `{"NextToken":"abcde","Buffer":"This is the buffer"}`, body.String())
+		assert.Equal(t, "the_bucket_name", *input.Bucket)
+		assert.Equal(t, "group/stream", *input.Key)
+		assert.Equal(t, "application/json", *input.ContentEncoding)
+		assert.Equal(t, int64(body.Len()), *input.ContentLength)
+		return nil, nil
+	}
+	client := &mockS3Client{}
+	registry := beater.NewS3Registry(client, "the_bucket_name")
+	registry.WriteStreamInfo(stream)
+}
+
+func Test_WriteStreamInfo_ShouldReturnError_OnError(t *testing.T) {
+	stream.Params.NextToken = aws.String("abcde")
+	stubPutObject = func(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+		return nil, errors.New("S3 Error")
+	}
+	client := &mockS3Client{}
+	registry := beater.NewS3Registry(client, "the_bucket_name")
+	err := registry.WriteStreamInfo(stream)
+	assert.Equal(t, "S3 Error", err.Error())
 }
